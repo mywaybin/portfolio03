895386b19995887510f023e6dbce149b41520422
mywaybin <118698699+mywaybin@users.noreply.github.com>
2022-12-16 14:53:37 +0900

    20221216

--- js/hb.js
+++ js/hb.js
@@ -11,21 +11,26 @@ $(function () {
         //arrows: false,
         dots: true,
         autoplay: true,
-        autoplaySpeed: 2000,
+        autoplaySpeed: 4000,
         pauseOnHover: false,
         pauseOnFocus: false
     });
 
-    $('.bottomBanner i').on('click', function () {
-        $('.bottomBanner .inner').slideToggle('on');
+    $('.mainSlide').on('afterChange', function (e, s, c) {
+        $('.main_dots li').eq(c).addClass('on')
+            .siblings().removeClass('on');
+        $('.mainVisual .main_num').text(c + 1)
     });
+
+    $('.main_dots li').on('click', function () {
+        var idx = $(this).index();
+        $('.mainSlide').slick('slickGoTo', idx)
+    })
 
     $('.main_pro_slide').slick({
         arrows: false,
-        dots: false,
+        dots: true,
         slidesToShow: 5,
-        autoplay: true,
-        autoplaySpeed: 2000,
     });
 
     $('.mainProduct .arrows .Prev').on('click', function () {
@@ -36,4 +41,20 @@ $(function () {
         $('.main_pro_slide').slick('slickNext')
     });
 
+})
+
+//tab menu
+
+$('.mainCustomer .tab_menu>li').on('click', function (event) {
+    event.preventDefault();
+    var _this = $(this); //이벤트가 적용된 자신을 반환
+    var idx = $(this).index(); //이벤트가 적용된 자신의 번호를 반환 0,1,2
+    _this.addClass('on').siblings().removeClass('on');
+    $('.mainCustomer .tab_content>li').eq(idx).addClass('on')
+        .siblings().removeClass('on')
 });
+
+$('#f_link').on('change', function () {
+    var link = $(this).val();
+    link && window.open(link);
+})
